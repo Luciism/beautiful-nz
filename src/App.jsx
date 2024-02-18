f608508635b54e844829c41eca53c0b65fe72449
odepleted <odepleted@gmail.com>
2024-02-18 15:45:50 +0000

    feat: added navbar

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { DataContext } from './DataContext';
+import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage/HomePage'
+
+function App() {
+  const supportedLanguages = ["en", "reo"];
+  let selectedLanguage = localStorage.getItem("lang");
+
+  if (!supportedLanguages.includes(selectedLanguage)) {
+    selectedLanguage = "en"; // set to default of english if language is invalid
+  }
+
+  const [lang, setLang] = useState(selectedLanguage);
+
+  return (
+    <DataContext.Provider value={{
+      lang: lang,
+      setLang: setLang
+    }}>
+      <Router>
+        <Navbar />
+        <div className="content">
+          
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+            </Routes>
+        </div>
+      </Router>
+    </DataContext.Provider>
+  );
+}
+
+export default App;
