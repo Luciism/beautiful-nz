6edaebc5c550419fa2789bc5d2592b37c9d12cd9
odepleted <odepleted@gmail.com>
2024-02-19 18:16:53 +0000

    feat: finished homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,19 @@
 import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 
+
+import { DataContext } from "../../DataContext";
 import './HomePage.css'
 import NewZealandTextImage from "../../assets/images/new-zealand-text.png";
-import SkewedRect from "../../assets/images/skewed-rect.png";
+import CardCultureImage from "../../assets/images/card-culture.png";
+import CardSceneryImage from "../../assets/images/card-scenery.png";
+import CardArt from "../../assets/images/card-art.png";
 
 
 const HomePage = () => {
+    const { lang } = useContext(DataContext);
+
     return (
         <div className='homepage'>
             <div className="intro-section">
@@ -17,19 +25,44 @@ const HomePage = () => {
                     <img src={NewZealandTextImage} alt="NEW ZEALAND AOTEAROA" />
                 </div>
             </div>
+
             <div className="cards-section">
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
+
+                <div className="card card-1">
+                    <img src={CardCultureImage} alt="Marae with people in front of it" />
+                    <div className="card-info">
+                        <h3>{lang == "en" ? "OUR CULTURE" : "TŌ TĀTOU TIKANGA"}</h3>
+                        <p>{lang == "en" ? "Uncover the treasures of Māori culture." : "Whakahorohia ngā Taonga o te Ao Māori."}</p>
+                        <Link to="" className='learn-more-btn pill-btn background-gradient-down'>{lang == "en" ? "LEARN MORE" : "WHAKAWHITI KIA MŌHIO"}</Link>
+                    </div>
+                </div>
+
+                <div className="card card-2">
+                    <img src={CardSceneryImage} alt="Lake with mountains in the distance" />
+                    <div className="card-info">
+                        <h3>{lang == "en" ? "DISCOVER SCENERY" : "WHIWHI KI TE WAI O TE WHENUA"}</h3>
+                        <p>{lang == "en" ? "Aotearoa's untamed wilderness." : "Te Ao Whakaharahara o Aotearoa."}</p>
+                        <Link to="" className='learn-more-btn pill-btn background-gradient-down'>{lang == "en" ? "LEARN MORE" : "WHAKAWHITI KIA MŌHIO"}</Link>
+                    </div>
+                </div>
+
+                <div className="card card-3">
+                    <img src={CardArt} alt="People looking at art in an art gallery." />
+                    <div className="card-info">
+                        <h3>{lang == "en" ? "ADMIRE ART" : "WHAKAMIHI KI TE TOI"}</h3>
+                        <p>{lang == "en" ? "Immerse yourself in artistic expression." : "Whakamihi ki te toi, huri ki te ao o te waiata."}</p>
+                        <Link to="" className='learn-more-btn pill-btn background-gradient-down'>{lang == "en" ? "LEARN MORE" : "WHAKAWHITI KIA MŌHIO"}</Link>
+                    </div>
+                </div>
+
             </div>
+
             <div className="tourism-guide-card">
-                <p className="heading-text">
-                    Planning a trip?
-                </p>
-                <p className="text">
-                    <span>View our tourism guide</span>
-                    <Icon icon="ep:right" />
-                </p>
+                <p className="heading-text">{lang == "en" ? "Planning a trip?" : "Ka hīoi rā koe i tētahi haerenga?"}</p>
+                <Link className="guide-link" to="">
+                    <span>{lang == "en" ? "View our tourism guide" : "Tirohia te tāringa haerenga a mātou"}</span>
+                    <Icon className='icon' icon="ep:right" />
+                </Link>
             </div>
         </div>
     );
